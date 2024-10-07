160cf85a300a35c70591ab7f1e8ef4d27e95ba17
munirayussuf <munirayussuf847@gmail.com>
2024-10-07 04:45:40 -0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+//hello world
+#include<stdio.h>
+int main()
+
+{
+	int number1;
+	int number2;
+	int result;
+	
+	result=number1+number2;
+	printf("enter 2 numbers");
+	scanf("%d",& numbers);
+	printf("the result is:%d.2f"result);
+}
